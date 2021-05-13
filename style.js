39dec7438511ec1684885e4e61f8fdedfeff1f3e
rockysam137 <lisnalia094@gmal.com>
2021-05-13 21:17:24 +0700

    first commit

---
+++ style.js
@@ -0,0 +1,10 @@
+//(starTyper)=div id di HTML
+$(function(){
+	$(".starTyper").typed({
+	strings: ["Internet, Technology, Music, Sport "],
+	typeSpeed : 50,
+	backDelay : 100.
+	loop : True,
+	loopCount: False,
+	});
+});
